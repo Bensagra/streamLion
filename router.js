a02560c22632f7b0e2a5852fa5fb3e5c6da16f83
Bensagra <bensagra@gmail.com>
2024-12-17 15:17:53 -0300

    fix: update file path for data.json in router.js

--- router.js
+++ router.js
@@ -9,7 +9,7 @@ const router = Router();
 
 router.get("/", (req, res) => {
     try {
-        let data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
+        let data = JSON.parse(fs.readFileSync('../data.json', 'utf-8'));
         res.json(data).status(200);
     } catch (error) {
         res.json({ error: error.message }).status(500);
