f9b0d521b0888ac3d6887ca003aa308d4ac89489
Bensagra <bensagra@gmail.com>
2024-12-17 15:34:16 -0300

    fix: simplify data loading in router.js by removing unnecessary path.resolve

--- src/router.js
+++ src/router.js
@@ -1,7 +1,4 @@
 import { Router } from "express";
-import path from 'path';
-
-
 import fs from "fs";
 
 
@@ -12,7 +9,7 @@ const router = Router();
 
 router.get("/", (req, res) => {
     try {
-        let data = JSON.parse(fs.readFileSync(path.resolve('./data.json'), 'utf-8'));
+        let data = JSON.parse(fs.readFileSync('./data.json', 'utf-8'));
         res.json(data).status(200);
     } catch (error) {
         res.json({ error: error.message }).status(500);
